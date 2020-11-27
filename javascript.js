b4fe34a2559a4af213f09eeec65c57236b509175
Thomas <thomashbusk@gmail.com>
2020-11-27 12:36:04 +0100

    Correct javascript code, missing styling

--- javascript.js
+++ javascript.js
@@ -7,30 +7,18 @@ let $2 = (bar) => {
     return document.getElementsByClassName(bar);
 }
 
-let days = []; //saves entire date day, month, year, time
-let bookingDate = []; //saves bookings made by people
-let bookingInfo = { //variable used to save booking info per person
-    month: "11",
-    day: "1"
-};
-
-//get date function
-let getDaysOfMonth = (year, month) => {
-    let bookingDate = new Date(year, month, 1);
-    while (bookingDate.getMonth() === month) {
-        days.push(new Date(bookingDate));
-        bookingDate.setDate(bookingDate.getDate() + 1);
-    }
-    return days;
+let bookingInfo = {
+    name: "Thomas",
+    amount: "4",
+    date: "",
+    time: ""
 }
+let bookingDate = [];
 
-//make list items
-let makeDates = (text) => {
-    let liDate = document.createElement("li");
-    liDate.setAttribute("class", "date");
-    liDate.setAttribute("id", "dateId" + text);
-    liDate.innerHTML = text;
-    return liDate;
+let showDate = () => {
+    for (let b of bookingDate) {
+        $("confirmationText").innerHTML = `Congratulations! you have booking a table on ${b.date} at ${b.time}, booked by ${b.name} with a party of ${b.amount} people.`;
+    }
 }
 
 //boolean local storage
@@ -45,59 +33,28 @@ const isLocalStorageEnabled = () => {
     }
 }
 
-//get date and save in local storage
-let pressDate = (e) => {
-    for (let i = 0; i < days.length; i++) {
-        let bk = Object.assign({}, bookingInfo);
-
-        $("dateId" + (i + 1)).addEventListener("click", () => {
-            $("dateId" + (i + 1)).style.backgroundColor = "blue";
-            let currentBookingDay = $("dateId" + (i + 1)).innerHTML;
-            bk.day = currentBookingDay;
-            bookingDate.push(bk);
-            console.log(bookingDate);
-            $("dateId" + (i + 1)).style.display = "none";
-        })
-    }
-
+let writeDate = () => {
     if (isLocalStorageEnabled) {
-        localStorage.removeItem("bookingDate");
-        let date = JSON.stringify(bookingDate);
-        localStorage.setItem("bookingDate", date);
-    }
-    console.log(localStorage.getItem("bookingDate"));
-}
+        $("confirmBtn").addEventListener("click", () => {
+            let getItem = localStorage.getItem('bookingDate');
+            let bookingDate = JSON.parse(getItem);
 
-//clicking on confirm btn prints out your confirmed booking information
-let confirmBooking = () => {
-    $("confirm").addEventListener("click", () => {
-        $("bookingConfirmation").innerHTML = "You have booked a table on the: " + bookingDate[bookingDate.length - 1].day + " of month: " + bookingDate[bookingDate.length - 1].month;
-    })
-}
+            let bk = Object.assign({}, bookingInfo);
+            bk.name = $("nameText").value;
+            bk.amount = $("amountText").value;
+            bk.date = $("date").value;
+            bk.time = $("time").value;
+            bookingDate.push(bk);
 
-//main function runs on load
-let doThis = () => {
-    getDaysOfMonth(2020, 10);
-    //open drop down menu
-    $("dropDownBtn").addEventListener("click", () => {
-        $("dropDown").classList.toggle("toggleOff");
-    });
-
-    //make all the list items
-    for (let i = 0; i < days.length; i++) {
-        $("dropDown").appendChild(makeDates(days[i].getDate()));
-    }
-
-    //change drop down btn text = clicked items element
-    for (let i = 0; i < days.length; i++) {
-        $2("date")[i].addEventListener("click", () => {
-            $("dropDownBtn").innerHTML = $2("date")[i].innerHTML;
+            let date = JSON.stringify(bookingDate);
+            localStorage.setItem("bookingDate", date);
         });
     }
-    pressDate();
-    
-    confirmBooking();
-    
+
 }
 
-window.addEventListener("load", doThis);
+let main = () => {
+    writeDate();
+}
+
+window.addEventListener("load", main);
